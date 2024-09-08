4d3fbd63dd0ea744283b29745f10d0cc8ff32346
Tangjj1996 <awesometangji@gmail.com>
2024-09-08 11:38:44 +0800

    feat: 添加服务端组件

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    GITHUB_TOKEN: string;
+  }
+}
